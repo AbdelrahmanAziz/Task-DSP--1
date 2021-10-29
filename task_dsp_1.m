c988831dd2b3fc839f0c02e59f137e84dc0c2da9
AbdelrahmanAziz <93330030+AbdelrahmanAziz@users.noreply.github.com>
2021-10-29 23:01:59 +0200

    Add files via upload

---
+++ task_dsp_1.m
@@ -0,0 +1,38 @@
+%%This are matrices
+A = [-7 5 -9; 2 -1 2; 1 -1 2];
+B = [16 3 2 13; 5 10 11 8; 9 6 7 12; 4 15 14 1];
+C = [4 2 -3; 7 -7 9; 3 -5 6];
+D = [6 3 2; 2 12 -7; -1 6 2; -5 15 11];
+
+%% Q 1 ANSWER
+(3*A) - (5*C);
+7*A+2*B;
+C*A;
+C * D';
+
+%% Q 2 ANSWER
+ zeros(n);
+  zeros(m,n);
+  ones(n);
+ones(m,n);
+size(D);
+zeros(size(D));
+diag([1 2 3 4]);
+eye(n);
+
+%% Q 3 ANSWER
+[A,B] ;
+[A;B];
+
+
+%% Q 4 ANSWER
+zeros(7,8);
+
+diag([5555555]);
+
+
+
+%% Q 5 ANSWER
+A(1,:);
+
+A(:,1);
